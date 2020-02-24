e9956791c304de4479c1ce21ab2d81fb8556410f
tcracked <alkoboy90@gmail.com>
2020-02-24 16:20:47 +0300

    stage 4

--- encryptdecrypt/Main.java
+++ encryptdecrypt/Main.java
@@ -2,14 +2,38 @@ package encryptdecrypt;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "we found a treasure!";
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c >= 'a' && c <= 'm') c = (char) ('z' - c + 'a');
-            else if  (c >= 'A' && c <= 'M') c = (char) ('Z' - c + 'A');
-            else if  (c >= 'n' && c <= 'z') c = (char) ('a' - c + 'z');
-            else if  (c >= 'N' && c <= 'Z') c = (char) ('a' - c + 'z');
-            System.out.print(c);
+        String operation = "enc";
+        String s = "";
+        int key = 0;
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "-mode":
+                    operation = args[i + 1];
+                    i++;
+                    break;
+                case "-data":
+                    s = args[i + 1];
+                    i++;
+                    break;
+                case "-key":
+                    key = Integer.parseInt(args[i + 1]);
+                    i++;
+                    break;
+            }
+        }
+        switch (operation) {
+            case "enc":
+                for (int i = 0; i < s.length(); i++) {
+                    char c = (char) (s.charAt(i) + key);
+                    System.out.print(c);
+                }
+                break;
+            case "dec":
+                for (int i = 0; i < s.length(); i++) {
+                    char c = (char) (s.charAt(i) - key);
+                    System.out.print(c);
+                }
+                break;
         }
     }
 }
